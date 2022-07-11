454b1d15e7a61e50b9349bafb104b173f30a0e45
jfranklin12 <j.franks725@gmail.com>
2022-07-11 10:04:03 -0400

    columns added and time set

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,5 @@
 var currentDayEl = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentDayEl)
+$("#currentDay").text(currentDayEl);
+
+var hour9El = moment().set('hour', 12).format("HH:mm");
+$("#hour").text(hour9El);
